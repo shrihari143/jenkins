8fb7f72f22019b6f3b5d0bf23c652b5b04848006
shrihari143 <126378618+shrihari143@users.noreply.github.com>
2025-07-09 22:43:54 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send("This project is deployed by CI/CD Jenkins");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
